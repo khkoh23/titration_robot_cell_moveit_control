66080f44b50d28c5da1e868174c9ae007f249e01
khkoh <khkoh23@gmail.com>
2025-11-14 09:37:14 +0800

    Added MoveState action server.Updated behavior tree, BT executor node.

---
+++ src/move_state_server.cpp
@@ -0,0 +1,100 @@
+#include <moveit/move_group_interface/move_group_interface.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include <titration_robot_interfaces/action/move_state.hpp>
+#include <string>
+
+class MoveStateActionServer : public rclcpp::Node {
+public:
+    using MoveState = titration_robot_interfaces::action::MoveState;
+    using GoalHandleMoveState = rclcpp_action::ServerGoalHandle<MoveState>;
+
+    explicit MoveStateActionServer(const rclcpp::NodeOptions & options = rclcpp::NodeOptions()) 
+    : Node("move_state_server", options), 
+    move_group_interface_(std::make_shared<moveit::planning_interface::MoveGroupInterface>(std::shared_ptr<rclcpp::Node>(this, [](auto){}), "ur_arm")) {
+        using namespace std::placeholders;
+        this->action_server_ = rclcpp_action::create_server<MoveState>(
+            this->get_node_base_interface(),
+            this->get_node_clock_interface(),
+            this->get_node_logging_interface(),
+            this->get_node_waitables_interface(),
+            "move_state",
+            std::bind(&MoveStateActionServer::handle_goal, this, _1, _2),
+            std::bind(&MoveStateActionServer::handle_cancel, this, _1),
+            std::bind(&MoveStateActionServer::handle_accepted, this, _1)
+        );
+        RCLCPP_INFO(this->get_logger(), "Move-state action server started");
+    }
+
+private:
+    std::shared_ptr<moveit::planning_interface::MoveGroupInterface> move_group_interface_;
+    rclcpp_action::Server<MoveState>::SharedPtr action_server_;
+    std::string home = "home";
+    std::string normal = "normal";
+
+    rclcpp_action::GoalResponse handle_goal(
+        const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const MoveState::Goal> goal) {
+        RCLCPP_INFO(this->get_logger(), "Received goal request with target named (%s)", goal->group_state.c_str());
+        (void)uuid;
+        // if ((goal->group_state != home) || (goal->group_state != normal)) { // To add goal reject criteria
+        //     return rclcpp_action::GoalResponse::REJECT;
+        // }
+        return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+    }
+
+    rclcpp_action::CancelResponse handle_cancel(const std::shared_ptr<GoalHandleMoveState> goal_handle) {
+        RCLCPP_INFO(this->get_logger(), "Received request to cancel goal");
+        (void)goal_handle;
+        return rclcpp_action::CancelResponse::ACCEPT;
+    }
+
+    void handle_accepted(const std::shared_ptr<GoalHandleMoveState> goal_handle) {
+        using namespace std::placeholders;
+        std::thread{std::bind(&MoveStateActionServer::execute, this, _1), goal_handle}.detach();
+    }
+
+    void execute(const std::shared_ptr<GoalHandleMoveState> goal_handle) {
+        RCLCPP_INFO(this->get_logger(), "Executing goal ...");
+        const auto goal = goal_handle->get_goal();
+        auto feedback = std::make_shared<MoveState::Feedback>();
+        auto result = std::make_shared<MoveState::Result>();
+        move_group_interface_->setNamedTarget(goal->group_state);
+        move_group_interface_->setMaxVelocityScalingFactor(0.5);
+        move_group_interface_->setMaxAccelerationScalingFactor(0.5);
+        moveit::planning_interface::MoveGroupInterface::Plan plan;
+        bool success = (move_group_interface_->plan(plan) == moveit::core::MoveItErrorCode::SUCCESS);
+
+        if(success) {
+            RCLCPP_INFO(this->get_logger(), "Move group plan - Succeeded");
+            moveit::core::MoveItErrorCode execute_success = move_group_interface_->execute(plan);
+            if (execute_success == moveit::core::MoveItErrorCode::SUCCESS) {
+                RCLCPP_INFO(this->get_logger(), "Move group execute - Succeeded");
+                result->outcome = true;
+                goal_handle->succeed(result);
+            }
+            else {
+                RCLCPP_ERROR(this->get_logger(), "Move group execute - Failed");
+                result->outcome = false;
+                goal_handle->abort(result);
+            }
+        }
+        else {
+            RCLCPP_ERROR(this->get_logger(), "Move group plan - Failed");
+            result->outcome = false;
+            goal_handle->abort(result);
+        }
+        // TODO: publish feedback
+    }
+
+};  // class MoveStateActionServer
+
+int main(int argc, char ** argv) {
+    rclcpp::init(argc, argv);
+    rclcpp::ExecutorOptions options;
+    rclcpp::NodeOptions node_options;
+    node_options.automatically_declare_parameters_from_overrides(true);
+    auto action_server = std::make_shared<MoveStateActionServer>(node_options);
+    rclcpp::spin(action_server);
+    rclcpp::shutdown();
+    return 0;
+}
--- src/titration_task.cpp
+++ src/titration_task.cpp
@@ -6,9 +6,12 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
 #include <titration_robot_interfaces/action/move_pose.hpp>
+#include <titration_robot_interfaces/action/move_state.hpp>
+#include <string>
 
 using namespace std::chrono_literals;
-using Action = titration_robot_interfaces::action::MovePose;
+using MovePoseAction = titration_robot_interfaces::action::MovePose;
+using MoveStateAction = titration_robot_interfaces::action::MoveState;
 
 enum class ActionResult : uint8_t {  // TODO: To match with rclcpp_action::ResultCode
     ActionNotCompleted,
@@ -17,45 +20,62 @@ enum class ActionResult : uint8_t {  // TODO: To match with rclcpp_action::Resul
     ActionSucceded
 };
 
+BT::NodeStatus IsTitrationDone() {
+    return BT::NodeStatus::FAILURE;
+}
+
+BT::NodeStatus IsPipetteEmpty() {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus DispenseStepVolume() {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus Aspire() {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus IsStateNormal() {
+    return BT::NodeStatus::FAILURE;
+}
+
+
 class CallMovePose : public BT::StatefulActionNode {
 public:
     CallMovePose(const std::string & action_name, const BT::NodeConfig & conf) : BT::StatefulActionNode(action_name, conf) {
-        node_ = rclcpp::Node::make_shared("action_client_node");
+        node_ = rclcpp::Node::make_shared("move_pose_action_client_node");
     }
 
     BT::NodeStatus onStart() {
         getInput<float>("position_x", position_x_);
         getInput<float>("position_y", position_y_);
         getInput<float>("position_z", position_z_);
-        getInput<float>("orientation_w", orientation_w_);
-        getInput<float>("orientation_x", orientation_x_);
-        getInput<float>("orientation_y", orientation_y_);
-        getInput<float>("orientation_z", orientation_z_);
         action_result_ = ActionResult::ActionNotCompleted;
         server_called_ = false;
-        client_ = rclcpp_action::create_client<Action>(node_, "move_pose");
+        client_ = rclcpp_action::create_client<MovePoseAction>(node_, "move_pose");
         return BT::NodeStatus::RUNNING;
     }
 
     BT::NodeStatus onRunning() {
         if (!server_called_) {
-            auto goal = Action::Goal();
+            auto goal = MovePoseAction::Goal();
             goal.position_x = position_x_;
             goal.position_y = position_y_;
             goal.position_z = position_z_;
-            goal.orientation_w = orientation_w_;
-            goal.orientation_x = orientation_x_;
-            goal.orientation_y = orientation_y_;
-            goal.orientation_z = orientation_z_;
+            goal.orientation_w = 0.0000;
+            goal.orientation_x = 0.7071;
+            goal.orientation_y = 0.7071;
+            goal.orientation_z = 0.0000;
             server_called_ = true;
             auto send_goal_future = client_->async_send_goal(goal);
             if (rclcpp::spin_until_future_complete(node_, send_goal_future) != rclcpp::FutureReturnCode::SUCCESS) {
-                std::cout << "Failed to send goal" << std::endl;
+                std::cout << "Failed to send move_pose goal" << std::endl;
                 return BT::NodeStatus::FAILURE;
             }
             auto goal_handle = send_goal_future.get();
             if (!goal_handle) {
-                std::cout << "Goal was rejected by server" << std::endl;
+                std::cout << "Goal was rejected by move_pose server" << std::endl;
                 return BT::NodeStatus::FAILURE;
             }
             client_->async_get_result(goal_handle, std::bind(&CallMovePose::result_callback, this, std::placeholders::_1));
@@ -65,22 +85,22 @@ public:
 
         switch (action_result_) {
             case ActionResult::ActionSucceded:
-                std::cout << "Call action: Succeded" << std::endl;
+                std::cout << "Call move_pose action: Succeded" << std::endl;
                 return BT::NodeStatus::SUCCESS;
                 break;
             case ActionResult::ActionFailed:
-                std::cout << "Call action: Failed" << std::endl;
+                std::cout << "Call move_pose action: Failed" << std::endl;
                 return BT::NodeStatus::FAILURE;
                 break;
             case ActionResult::ActionCancelled:
-                std::cout << "Call action : Cancelled" << std::endl;
+                std::cout << "Call move_pose action : Cancelled" << std::endl;
                 return BT::NodeStatus::FAILURE;
                 break;
             case ActionResult::ActionNotCompleted:
                 break;
         }
 
-        std::cout << "Call action: Waiting server execution" << std::endl;
+        std::cout << "Call move_pose action: Waiting server execution" << std::endl;
         return BT::NodeStatus::RUNNING;
     }
 
@@ -91,21 +111,17 @@ public:
             BT::InputPort<float>("position_x"),
             BT::InputPort<float>("position_y"),
             BT::InputPort<float>("position_z"),
-            BT::InputPort<float>("orientation_w"),
-            BT::InputPort<float>("orientation_x"),
-            BT::InputPort<float>("orientation_y"),
-            BT::InputPort<float>("orientation_z"),
         };
     }
 
 private:
     rclcpp::Node::SharedPtr node_;
-    rclcpp_action::Client<Action>::SharedPtr client_;
+    rclcpp_action::Client<MovePoseAction>::SharedPtr client_;
     float position_x_, position_y_, position_z_, orientation_w_, orientation_x_, orientation_y_, orientation_z_;
     bool server_called_;
     ActionResult action_result_;
 
-    void result_callback(const rclcpp_action::ClientGoalHandle<Action>::WrappedResult & result) {
+    void result_callback(const rclcpp_action::ClientGoalHandle<MovePoseAction>::WrappedResult & result) {
         switch (result.code) {
             case rclcpp_action::ResultCode::SUCCEEDED:
                 action_result_ = ActionResult::ActionSucceded;
@@ -120,7 +136,99 @@ private:
                 break;
         }
     }
-};
+}; // class CallMovePose
+
+
+
+
+class CallMoveState : public BT::StatefulActionNode {
+public:
+    CallMoveState(const std::string & action_name, const BT::NodeConfig & conf) : BT::StatefulActionNode(action_name, conf) {
+        node_ = rclcpp::Node::make_shared("move_state_action_client_node");
+    }
+
+    BT::NodeStatus onStart() {
+        getInput<std::string>("group_state", group_state_);
+        action_result_ = ActionResult::ActionNotCompleted;
+        server_called_ = false;
+        client_ = rclcpp_action::create_client<MoveStateAction>(node_, "move_state");
+        return BT::NodeStatus::RUNNING;
+    }
+
+    BT::NodeStatus onRunning() {
+        if (!server_called_) {
+            auto goal = MoveStateAction::Goal();
+            goal.group_state = group_state_;
+            server_called_ = true;
+            auto send_goal_future = client_->async_send_goal(goal);
+            if (rclcpp::spin_until_future_complete(node_, send_goal_future) != rclcpp::FutureReturnCode::SUCCESS) {
+                std::cout << "Failed to send move_state goal" << std::endl;
+                return BT::NodeStatus::FAILURE;
+            }
+            auto goal_handle = send_goal_future.get();
+            if (!goal_handle) {
+                std::cout << "Goal was rejected by move_state server" << std::endl;
+                return BT::NodeStatus::FAILURE;
+            }
+            client_->async_get_result(goal_handle, std::bind(&CallMoveState::result_callback, this, std::placeholders::_1));
+        }
+
+        rclcpp::spin_some(node_);
+
+        switch (action_result_) {
+            case ActionResult::ActionSucceded:
+                std::cout << "Call move_state action: Succeded" << std::endl;
+                return BT::NodeStatus::SUCCESS;
+                break;
+            case ActionResult::ActionFailed:
+                std::cout << "Call move_state action: Failed" << std::endl;
+                return BT::NodeStatus::FAILURE;
+                break;
+            case ActionResult::ActionCancelled:
+                std::cout << "Call move_state action : Cancelled" << std::endl;
+                return BT::NodeStatus::FAILURE;
+                break;
+            case ActionResult::ActionNotCompleted:
+                break;
+        }
+
+        std::cout << "Call move_state action: Waiting server execution" << std::endl;
+        return BT::NodeStatus::RUNNING;
+    }
+
+    void onHalted() { return; }
+
+    static BT::PortsList providedPorts() {
+        return {
+            BT::InputPort<std::string>("group_state"),
+        };
+    }
+
+private:
+    rclcpp::Node::SharedPtr node_;
+    rclcpp_action::Client<MoveStateAction>::SharedPtr client_;
+    std::string group_state_;
+    bool server_called_;
+    ActionResult action_result_;
+
+    void result_callback(const rclcpp_action::ClientGoalHandle<MoveStateAction>::WrappedResult & result) {
+        switch (result.code) {
+            case rclcpp_action::ResultCode::SUCCEEDED:
+                action_result_ = ActionResult::ActionSucceded;
+                break;
+            case rclcpp_action::ResultCode::ABORTED:
+                action_result_ = ActionResult::ActionFailed;
+                break;
+            case rclcpp_action::ResultCode::CANCELED:
+                action_result_ = ActionResult::ActionCancelled;
+                break;
+            default:
+                break;
+        }
+    }
+}; // class CallMoveState
+
+
 
 class BTExecutor : public rclcpp::Node {
 public:
@@ -139,9 +247,13 @@ private:
 
     void init_btree() {
         blackboard_->set<rclcpp::Node::SharedPtr>("node", this->shared_from_this());
-        factory_.registerNodeType<CallMovePose>("MovePoseReady");
-        factory_.registerNodeType<CallMovePose>("MovePoseA1");
-        factory_.registerNodeType<CallMovePose>("MovePoseA2");
+        factory_.registerNodeType<CallMovePose>("MovePose");
+        factory_.registerSimpleCondition("IsTitrationDone", std::bind(IsTitrationDone));
+        factory_.registerSimpleCondition("IsPipetteEmpty", std::bind(IsPipetteEmpty));
+        factory_.registerSimpleCondition("DispenseStepVolume", std::bind(DispenseStepVolume));
+        factory_.registerSimpleCondition("Aspire", std::bind(Aspire));
+        factory_.registerSimpleCondition("IsStateNormal", std::bind(IsStateNormal));
+        factory_.registerNodeType<CallMoveState>("MoveState");
         this->declare_parameter<std::string>("tree_xml_file", "");
         std::string tree_file;
         this->get_parameter("tree_xml_file", tree_file);
@@ -157,6 +269,8 @@ private:
     }
 
 }; // class BTExecutor
+
+
 
 int main(int argc, char * argv[]) {
     rclcpp::init(argc, argv);
